c05b37cb9dd3f13ddb76df078fa388333533c6c1
Jordan Gillard <jordan-gillard@outlook.com>
2020-04-25 21:56:16 +0200

    refactor: Add hospital name as key, value to questions get response

--- src/python/server/routes.py
+++ src/python/server/routes.py
@@ -10,6 +10,7 @@ def get_questions(hospital_id):
         questions = [question.serialize() for question in hospital.questions if question.active]
         return jsonify({
             'success': True,
+            'hospital_name': hospital.name,
             'questions': questions,
             'total_questions': len(questions)
         })
