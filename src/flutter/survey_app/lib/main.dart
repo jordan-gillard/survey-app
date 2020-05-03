ed921edd10597c1b5c9c1ffcd18b725cb5ec4405
Jordan Gillard <jordan-gillard@outlook.com>
2020-05-03 17:20:37 +0200

    feat: Add thank you screen after response POSTed to server

--- src/flutter/survey_app/lib/main.dart
+++ src/flutter/survey_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:surveyapp/screens/loading_screen.dart';
 import 'package:surveyapp/screens/sending_screen.dart';
 import 'package:surveyapp/screens/survey_page.dart';
+import 'package:surveyapp/screens/thank_you.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       routes: {
         '/': (context) => LoadingScreen(),
+//        '/': (context) => ThankYouScreen(),
         '/survey': (context) => SurveyPage(),
         '/sendingScreen': (context) => SendingScreen()
       },
