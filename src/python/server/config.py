f6f11a32cced216290933395c85a753eb98fa6f2
Jordan Gillard <jordan-gillard@outlook.com>
2020-05-13 16:20:30 +0200

    feat: Adds pyodcd and ODBC database connector support

--- src/python/server/config.py
+++ src/python/server/config.py
@@ -1,9 +1,14 @@
 import os
+import urllib
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
+ODBC_connection_string = os.environ.get('ODBC_CONNECTION_STRING')
+params = urllib.parse.quote_plus(ODBC_connection_string)
+
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SURVEY_APP_DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = "mssql+pyodbc:///?odbc_connect=%s" % params
     SQLALCHEMY_TRACK_MODIFICATIONS = False
